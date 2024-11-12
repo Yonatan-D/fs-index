0cf574083374f972920072e9463f6127775a2b7f
yonatan <yonatan.lin@outlook.com>
2024-11-12 10:22:12 +0800

    feat: 删除未使用的工具类方法 - 删除 `utils` 文件中的 `formatSize` 和 `formatDate` 方法 - 这些方法在项目中没有被引用 - 删除这些方法可以减少代码冗余，提高代码可维护性

--- src/utils.js
+++ src/utils.js
@@ -2,27 +2,6 @@ const dayjs = require('dayjs');
 
 const utils = {
   /**
-   * format file size
-   * @param {Number} bytes
-   * @return {String} such: 10 KB
-   */
-  formatSize: (bytes) => {
-    const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return '0 B';
-    const index = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return (bytes / Math.pow(1024, index)).toFixed(1) + ' ' + sizes[index];
-  },
-
-  /**
-   * format file modification time
-   * @param {Date} date 
-   * @returns {String} such: 2024-10-16 09:30:01
-   */
-  formatDate: (date) => {
-    return dayjs(date).format('YYYY-MM-DD HH:mm:ss');
-  },
-
-  /**
    * 合并中间件
    * @param {} middlewares 
    * @returns 
