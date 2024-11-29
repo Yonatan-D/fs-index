682a48bc3bb2b0a35141a614e48f5813cdb17ccb
yonatan <yonatan.lin@outlook.com>
2024-11-29 18:07:52 +0800

    refactor: 优化服务入口可读性

--- server.js
+++ server.js
@@ -6,31 +6,30 @@ import toml from 'toml';
 import loadModules from './src/middleware/index.js';
 import loadExtends from './src/extends/index.js';
 
+async function createServer() {
+  const app = express();
 
-const app = express();
-
-
-// 检查文件路径，不存在就创建
-const ensureFilePath = (filepath) => {
-  console.log(`Ensures Directory: ${filepath}`);
-  fs.ensureDirSync(filepath);
-}
-
-// 加载配置
-const loadConfig = (cfgFile) => {
-  try {
-    const cfgContent = toml.parse(fs.readFileSync(cfgFile, 'utf8'));
-    // console.log(cfgContent);
-    
-    console.log(`Config: ${cfgFile}`);
-  } catch (error) {
-    console.log(`Config: 读取配置文件失败 (${error.message})`);
+  // 检查文件路径，不存在就创建
+  const ensureFilePath = (filepath) => {
+    console.log(`Ensures Directory: ${filepath}`);
+    fs.ensureDirSync(filepath);
   }
-}
 
-// 初始化
-async function init() {
+  // 加载配置
+  const loadConfig = (cfgFile) => {
+    try {
+      const cfgContent = toml.parse(fs.readFileSync(cfgFile, 'utf8'));
+      // console.log(cfgContent);
+      
+      console.log(`Config: ${cfgFile}`);
+    } catch (error) {
+      console.log(`Config: 读取配置文件失败 (${error.message})`);
+    }
+  }
+
+  // 初始化
   await loadExtends(app);
+  await loadModules(app);
 
   const {
     config,
@@ -49,13 +48,13 @@ async function init() {
     console.log(c.bgRed('[fsIndex] init - 系统启动异常'));
     app.MyAPI.Throw(error);
   }
+
+  return { app };
 }
 
-init().then(() => {
+createServer().then(({ app }) => {
   const { location, resource } = app.MyAPI.GlobalData;
 
-  loadModules(app);
-  
   // 0.0.0.0: 强制节点服务器使用Ipv4侦听
   // 如果要兼容 IPv6, 就不能配置 0.0.0.0, 而是检查并移除 ::ffff: 前缀
   app.listen(location.port, '0.0.0.0', () => {
