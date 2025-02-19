b36c88a7e0941a6ca4fca9bd9535ba9639b4d420
yonatan <yonatan.lin@outlook.com>
2025-02-19 11:38:23 +0800

    wip: FileList component

--- src/web/widgets/file.js
+++ src/web/widgets/file.js
@@ -1,27 +1,116 @@
+/**
+ * 日期格式化 
+ * @param {string} dateStr
+ * @returns {string} 格式: YYYY-MM-DD HH:mm:ss
+ */
+const formatDate = (dateStr) => {
+  if (!dateStr) return dateStr;
+
+  const date = new Date(dateStr);
+  // 获取年、月、日、小时、分钟、秒
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从0开始，所以需要加1
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  // 组合成所需格式
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+/**
+ * 日期时间转最近时间提示
+ * @param {string} datetime 日期时间, 格式: YYYY-MM-DD HH:mm:ss
+ */
+const datetime2latest = (datetime) => {
+  if (!datetime) return '';
+  datetime = datetime.replace(/-/g, '/');
+
+  let curTimestamp = Math.ceil(new Date().getTime() / 1000); // 当前的时间戳，不含毫秒
+  let recTimestamp = Math.ceil(new Date(datetime).getTime() / 1000); // 传入的时间戳，不含毫秒
+
+  const timestamp = curTimestamp - recTimestamp; // 时间戳差值
+
+  // 1小时内
+  if (timestamp < 3600) {
+    return Math.ceil((timestamp) / 60) + '分钟前';
+  }
+
+  // 1天内
+  if (timestamp < 3600 * 24) {
+    return Math.ceil((timestamp) / 3600) + '小时前';
+  }
+
+  const curDate = new Date();
+  const curYear = curDate.getFullYear();
+  const curMonth = String(curDate.getMonth() + 1).padStart(2, '0');
+  const curDay = String(curDate.getDate()).padStart(2, '0');
+  curTimestamp = new Date(`${curYear}/${curMonth}/${curDay} 00:00:00`).getTime();
+
+  const date = new Date(datetime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+  recTimestamp = new Date(`${year}/${month}/${day} 00:00:00`).getTime();
+
+  // 昨天
+  if (curTimestamp === recTimestamp + 24 * 3600 * 1000) {
+    return `昨天 ${hour}:${minute}`;
+  }
+
+  // 前天
+  if (curTimestamp === recTimestamp + 24 * 3600 * 1000 * 2) {
+    return `前天 ${hour}:${minute}`;
+  }
+
+  // 直接返回输入的日期字符串
+  return datetime;
+}
+
+/**
+ * 文件大小格式化
+ * @param {number} bytes
+ * @return {string} 例如: 10 KB
+ */
+const formatSize = (bytes) => {
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes == 0) return '0 B';
+  const index = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return (bytes / Math.pow(1024, index)).toFixed(1) + ' ' + sizes[index];
+}
+
 class FileList extends HTMLElement {
   constructor() {
     super();
 
-    // 创建 Shadow DOM
     const shadow = this.attachShadow({ mode: 'open' });
-
-    // 创建一个 slot 元素
     const slot = document.createElement('slot');
-
-    // 将 slot 添加到 Shadow DOM 中
     shadow.appendChild(slot);
 
-    // 你可以通过 slot.assignedNodes() 获取插入的内容
+
     const nodes = slot.assignedNodes();
-
-    // 过滤出 a 标签
     const list = this.filterFileList(nodes);
-    console.log(list); // 这将输出 a 标签的列表
 
-    Array.from(list).forEach((item) => {
-      console.log(item);
-      
-    })
+    Array.from(list).forEach((el) => {
+      // 移除 title 属性
+      el.removeAttribute('title');
+
+      // 格式化日期
+      const dateEl = el.querySelector('span.date');
+      const dateStr = formatDate(dateEl.innerHTML);
+      dateEl.setAttribute('title', dateStr);
+      dateEl.style.direction = 'ltr';
+      dateEl.innerHTML = datetime2latest(dateStr);
+
+      // 如果不是目录，格式化文件大小
+      if (!el.className.includes('icon-directory')) {
+        const sizeEl = el.querySelector('span.size');
+        sizeEl.style.direction = 'ltr';
+        sizeEl.innerHTML = formatSize(sizeEl.innerHTML);
+      }
+    });
   }
 
   filterFileList(nodes) {
