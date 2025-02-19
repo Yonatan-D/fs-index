12e7e91bb880ad122e41b0d3fee13a583a1298f9
yonatan <yonatan.lin@outlook.com>
2025-02-19 18:10:29 +0800

    feat: 删除旧的插件和api初始化方法

--- src/web/components/index.js
+++ src/web/components/index.js
@@ -1,17 +1,3 @@
-const widgets = [
-  '/search.js',
-  '/contextMenu.js',
-]
-
-export function initWidgets() {
-  widgets.forEach(widget => {
-    const script = document.createElement('script');
-    script.src = widget;
-    script.type = 'text/babel';
-    document.body.appendChild(script);
-  })
-}
-
 export function saveCookie() {
   // 将url携带的token参数设置到Cookie
   const searchParams = new URLSearchParams(window.location.search);
@@ -21,36 +7,4 @@ export function saveCookie() {
   }
 }
 
-export function initApis() {
-  window.$app = {
-    ...window.$app,
-    dom: {
-      create: (id, component) => {
-        const rootNode = document.createElement(id);
-        const root = ReactDOM.createRoot(rootNode);
-        root.render(React.createElement(component));
-        return rootNode;
-      },
-  
-      appendTo: (selector, node) => {
-        document.querySelector(selector).appendChild(node);
-      }
-    }
-  }
-}
-
-export function installWidgets() {
-  const flag = setInterval(() => {
-    if ($app.widgets?.length > 0) {
-      $app.widgets.forEach(installFn => {
-        typeof installFn === 'function' && installFn();
-      })
-      clearInterval(flag);
-    }
-  }, 10)
-}
-
-initApis();
-initWidgets();
 saveCookie();
-installWidgets();
